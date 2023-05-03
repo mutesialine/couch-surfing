dc1b7d8a1b59f68563a3bb6aeaf79ec374a1317a
Mutesi <ktesialine@gmail.com>
2023-05-03 20:31:41 +0200

    feat: setting up files

---
+++ index.ts
@@ -0,0 +1,26 @@
+let reviewTotalDisplay = document.querySelector("#reviews");
+const reviews = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
+
+function showReviewTotal(value: number) {
+  reviewTotalDisplay?.innerHTML = "total review" + value.toString();
+}
+showReviewTotal(reviews.length);
